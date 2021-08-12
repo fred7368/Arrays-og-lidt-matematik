13c0c8ade584a0057d4926b697a24f65821fd588
zbcfoha <zbcfoha@ZBC-S-6BN21>
2021-08-12 09:33:44 +0200

    Add project files.

---
+++ Arrays og lidt matematik/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Arrays_og_lidt_matematik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 2, 4, 6, 8, 10, 12, 14, 16, 18 };
+
+            
+        }
+    }
+}
