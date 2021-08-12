dc86823f8c5eb7bfd2d6deb698681a1264a34e52
Frederik Oliver Høyer-Andersen <fred7368@zbc.dk>
2021-08-12 13:06:48 +0200

    -

--- Arrays og lidt matematik/Program.cs
+++ Arrays og lidt matematik/Program.cs
@@ -7,9 +7,34 @@ namespace Arrays_og_lidt_matematik
     {
         static void Main(string[] args)
         {
-            List<int> numbers = new List<int> { 2, 4, 6, 8, 10, 12, 14, 16, 18 };
+            List<int> numbers = new List<int>();
+            for(int i = 2; i<= 20; i = i + 2)
+            {
+                numbers.Add(i);
 
-            int i = 7;
+            }
+            PrintList(numbers, "just created");
+            for (int i = 0; i< numbers.Count; i++)
+            {
+                if(numbers[i] % 3 == 0)
+                {
+                    numbers.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            PrintList(numbers, "Removed those that mod 3");
+
+
+
+        }
+        static void PrintList(List<int> list,string label)
+        {
+            Console.WriteLine(label);
+            foreach(int tmp in list)
+            {
+                Console.WriteLine(tmp);
+            }
         }
     }
 }
